a246911646f02793fabf255140cd06979acbb58d
Ella475 <Ellashalom1995@gmail.com>
2023-03-01 18:56:01 +0200

    added all datasets

--- preprocess/preprocessing_credit.py
+++ preprocess/preprocessing_credit.py
@@ -4,60 +4,32 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder
 
 
-class Mode(Enum):
-    TRAIN = 'train'
-    TEST = 'test'
-
-
-def load_and_preprocess_credit(mode: Mode = Mode.TRAIN, path: str = "./datasets/credit") -> pd.DataFrame:
+def load_and_preprocess_credit(path: str = "./datasets/credit") -> pd.DataFrame:
     gender_map = {"'male single'": "male", "'female div/dep/mar'": "female", "'male mar/wid'": "male",
                   "'male div/sep'": "male"}
     status_map = {"'male single'": "single", "'female div/dep/mar'": "married/wid/sep",
                   "'male mar/wid'": "married/wid/sep", "'male div/sep'": "married/wid/sep"}
-    column_names = ['Age', 'Sex', 'Job', 'Housing', 'Saving accounts', 'Checking account', 'Credit amount', 'Duration',
-                    'Purpose', 'Risk']
 
-    if mode == Mode.TRAIN:
-        path = path + "_data.csv"
-        # Load train data
-        dt = pd.read_csv(path, header=None, names=column_names,
-                         skipinitialspace=True, na_values='?')
-    elif mode == Mode.TEST:
-        path = path + "_test.csv"
-        # Load test data
-        dt = pd.read_csv(path, header=0, names=column_names,
-                         skipinitialspace=True, na_values='?')
-    else:
-        raise ValueError('mode must be either train or test')
+    dt = pd.read_csv(path, header=0, skipinitialspace=True, na_values='?')
+    dt.dropna(inplace=True)
+    dt.reset_index(drop=True, inplace=True)
+    categorical_cols = ['checking_status', 'credit_history', 'purpose',
+                        'savings_status', 'employment', 'personal_status',
+                        'other_parties', 'property_magnitude', 'other_payment_plans',
+                        'housing', 'job', 'own_telephone', 'foreign_worker']
+    numerical_cols = ['duration', 'credit_amount', 'installment_commitment',
+                    'residence_since', 'age', 'existing_credits', 'num_dependents']
 
-    dt['sex'] = dt['personal_status'].replace(gender_map)
-    dt['civil_status'] = dt['personal_status'].replace(status_map)
-    dt.drop(columns=["personal_status"], inplace=True)
-    dt.rename(columns={"credit": "class"}, inplace=True)
-    dt['class'] = dt['class'].replace({"good": "P", "bad": "N"})
-    # Convert categorical variables to numerical labels
-    le = LabelEncoder()
-    dt['Sex'] = le.fit_transform(dt['Sex'])
-    dt['Housing'] = le.fit_transform(dt['Housing'])
-    dt['Saving accounts'] = le.fit_transform(dt['Saving accounts'].astype(str))
-    dt['Checking account'] = le.fit_transform(dt['Checking account'].astype(str))
-    dt['Purpose'] = le.fit_transform(dt['Purpose'])
+    dt['class'] = dt['class'].map({'bad': 0, 'good': 1})
 
-    # Convert numerical labels to one-hot encoding
-    ohe = OneHotEncoder(sparse=False)
-    housing_ohe = ohe.fit_transform(dt[['Housing']])
-    dt[['Housing_0', 'Housing_1', 'Housing_2']] = pd.DataFrame(housing_ohe, index=dt.index)
+    encoder = OneHotEncoder()
+    encoded = encoder.fit_transform(dt[categorical_cols])
+    dt_encoded = pd.DataFrame(encoded.toarray(), columns=encoder.get_feature_names_out(categorical_cols))
 
-    # Drop the original categorical variables
-    dt.drop(['Housing'], axis=1, inplace=True)
-
-    # Impute missing values in 'Saving accounts' and 'Checking account'
-    dt['Saving accounts'].fillna(value='unknown', inplace=True)
-    dt['Checking account'].fillna(value='unknown', inplace=True)
-
-    # Scale the numerical variables
+    dt[numerical_cols] = dt[numerical_cols].astype(int)
     scaler = StandardScaler()
-    dt[['Age', 'Job', 'Credit amount', 'Duration']] = scaler.fit_transform(
-        dt[['Age', 'Job', 'Credit amount', 'Duration']])
+    scaled = scaler.fit_transform(dt[numerical_cols])
+    dt_scaled = pd.DataFrame(scaled, columns=numerical_cols)
 
-    return dt
+    dt_preprocessed = pd.concat([dt_encoded, dt_scaled, dt["class"]], axis=1, ignore_index=True)
+    return dt_preprocessed
--- preprocess/preprocessing_heart.py
+++ preprocess/preprocessing_heart.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
@@ -8,32 +8,23 @@ from preprocess.preprocessing_adult import Mode
 
 
 def load_and_preprocess_heart(mode: Mode = Mode.TRAIN, path: str = "./datasets/heart") -> pd.DataFrame:
-    column_names = ['age', 'sex', 'chest_pain_type', 'resting_blood_pressure', 'cholesterol', 'fasting_blood_sugar',
-                    'rest_ecg', 'max_heart_rate_achieved',
-                    'exercise_induced_angina', 'st_depression', 'st_slope', 'num_major_vessels', 'thalassemia', 'class']
+    dt = pd.read_csv(path, header=0, skipinitialspace=True, na_values='?')
+    dt.dropna(inplace=True)
+    dt.reset_index(drop=True, inplace=True)
 
-    if mode == Mode.TRAIN:
-        path = path + "_data.csv"
-        # Load train data
-        dt = pd.read_csv(path, header=None, names=column_names,
-                         skipinitialspace=True, na_values='?')
-    elif mode == Mode.TEST:
-        path = path + "_test.csv"
-        # Load test data
-        dt = pd.read_csv(path, header=0, names=column_names,
-                         skipinitialspace=True, na_values='?')
-    else:
-        raise ValueError('mode must be either train or test')
+    categorical_cols = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'thal']
+    numerical_cols = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak', 'ca']
 
-    # Convert categorical features to numerical labels
-    cat_cols = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'thal']
-    for col in cat_cols:
-        le = LabelEncoder()
-        dt[col] = le.fit_transform(dt[col].astype(str))
+    # rename 'target' column to 'class'
+    dt.rename(columns={'target': 'class'}, inplace=True)
 
-    # Normalize numeric features
-    num_cols = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak', 'ca']
+    dt[numerical_cols] = dt[numerical_cols].astype(int)
     scaler = StandardScaler()
-    dt[num_cols] = scaler.fit_transform(dt[num_cols])
+    scaled = scaler.fit_transform(dt[numerical_cols])
+    dt_scaled = pd.DataFrame(scaled, columns=numerical_cols)
+
+    encoder = OneHotEncoder()
+    encoded = encoder.fit_transform(dt[categorical_cols])
+    dt_encoded = pd.DataFrame(encoded.toarray(), columns=encoder.get_feature_names_out(categorical_cols))
 
     return dt
