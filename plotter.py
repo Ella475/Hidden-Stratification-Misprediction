fa2befc70bc2d85d9375d68d71d75bba1d32cd59
Ella Shalom <Ella.shalom.w@gmail.com>
2023-03-02 23:57:14 +0200

    add example.ipynb

--- plotter.py
+++ plotter.py
@@ -8,10 +8,6 @@ mpl.use('TkAgg')
 
 from configs.expriment_config import DatasetNames, InputMode, ClusteringMethods, EvaluationMethods
 
-divergence_dir = Path('results/divergence_plot')
-tsne_dir = Path('results/tsne')
-imbalance_dir = Path('results/imbalance_plot')
-
 
 def show_pngs(pngs_path: List[Path]) -> None:
     for png_path in pngs_path:
@@ -20,6 +16,27 @@ def show_pngs(pngs_path: List[Path]) -> None:
         plt.imshow(img)
 
     plt.show()
+
+
+def get_plots_path(experiment_name: str, class_num: int) -> List[Path]:
+    tsne_dir = Path('results/tsne')
+    divergence_dir = Path('results/divergence_plot')
+    imbalance_dir = Path('results/imbalance_plot')
+
+    tsne_path = tsne_dir / f'{experiment_name}_{class_num}.png'
+    divergence_path = divergence_dir / f'{experiment_name}_{class_num}.png'
+    imbalance_path = imbalance_dir / f'{experiment_name}_{class_num}.png'
+
+    if not tsne_path.exists() or not divergence_path.exists():
+        print('There are no plots for this experiment')
+        exit(1)
+
+    png_paths = [tsne_path, divergence_path]
+
+    if imbalance_path.exists():
+        png_paths.append(imbalance_path)
+
+    return png_paths
 
 
 if __name__ == '__main__':
@@ -34,18 +51,7 @@ if __name__ == '__main__':
     experiment_name = f'{dataset_name.get_value()}_{input_mode.get_value()}_{clustering_method.get_value()}_{eval_method.get_value()}{"_only_cluster" if stop_after_clustering else ""}'
     print(f'Running experiment: {experiment_name}')
 
-    tsne_path = tsne_dir / f'{experiment_name}_{class_num}.png'
-    divergence_path = divergence_dir / f'{experiment_name}_{class_num}.png'
-    imbalance_path = imbalance_dir / f'{experiment_name}_{class_num}.png'
-
-    if not tsne_path.exists() or not divergence_path.exists():
-        print('There are no plots for this experiment')
-        exit(1)
-
-    png_paths = [tsne_path, divergence_path]
-
-    if imbalance_path.exists():
-        png_paths.append(imbalance_path)
+    png_paths = get_plots_path(experiment_name, class_num)
 
     show_pngs(png_paths)
 
